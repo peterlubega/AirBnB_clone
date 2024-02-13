886837b2ba5c3542e3f2525cabb5bc1afe7b5a87
peterlubega <*peterlubega100@gmail.com>
2024-02-12 23:19:51 -0800

    fix: staorage file and base models

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,44 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+"""This module defines a base class for all models in our hbnb clone"""
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
-
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        """Date and time format for parsing strings"""
-        DT_format = "%Y-%m-%dT%H:%M:%S.%f"
-
-        """Generate a unique ID using uuid4()"""
-        self.id = str(uuid4())
-
-        """If keyword arguments are provided, process them"""
-        if kwargs:
-            for key, value in kwargs.items():
-                try:
-                    """Use setattr for dynamic attribute assignment"""
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(value, DT_format))
-                    else:
-                        setattr(self, key, value)
-                except ValueError:
-                    """Handle datetime parsing errors gracefully"""
-                    print(f"Error parsing {key}: {value}. Skipping.")
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
         else:
-            """If no args, set created_at & updated_at to current datetime"""
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            models.storage.new(self)
-
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair class_name representing
-        the class name of the object.
-        """
-        result_dict = self.__dict__.copy()
-        result_dict["created_at"] = self.created_at.isoformat()
-        result_dict["updated_at"] = self.updated_at.isoformat()
-        result_dict["class_name"] = self.__class__.__name__
-        return result_dict
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
